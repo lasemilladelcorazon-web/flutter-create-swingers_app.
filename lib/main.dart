5fa216d14445d3012ad1c401ef2bf3954a5dd049
lasemilladelcorazon-web <lasemilladelcorazon@gmail.com>
2026-01-04 17:20:22 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,34 @@
 
+// Esta es la pantalla que verán después de pagar
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [
+    const LoungeScreen(), // El Muro
+    const ProfileScreen(), // El Perfil
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        selectedItemColor: const Color(0xFFD4AF37),
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedIndex,
+        onTap: (index) => setState(() => _selectedIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.explore), label: "Lounge"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Perfil"),
+        ],
+      ),
+    );
+  }
+}
